734ea6e5dd96893f5ac0c922a80f2695fab1fbff
juliasarah <36396636+juliasarah@users.noreply.github.com>
2018-03-07 20:04:49 +0530

    Create roman to integer.py

---
+++ roman to integer.py
@@ -0,0 +1,15 @@
+val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 1000}
+
+def main():
+    string = str(raw_input(''))
+    string = string.upper()
+    total = 0
+    while string:
+        if len(string) == 1 or val[string[0]] >= val[string[1]]:
+            total += val[string[0]]
+            string = string[1:]
+        else:
+            total += val[string[1]] - val[string[0]]
+            string = string[2:]
+    print total
+main()
